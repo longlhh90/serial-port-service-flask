39732b7ea6c42b4f044f013b5d5d423480ff657c
Lukas Le <longlhh90@google.com>
2021-07-14 17:45:05 -0600

    add serial port services

--- app/main/services/serial_port_services.py
+++ app/main/services/serial_port_services.py
@@ -1,17 +1,121 @@
+from app.serialcore import get_list_ports, init_serial_port, write_to_serial_port
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from app.init.api import api
+from app.utils import Collections
 import serial
-import serial.tools.list_ports as port_list
 
 
-def get_list_ports():
-    ports = list(port_list.comports())
-    for p in ports:
-        print(p)
-    return ports
+# HELPERS
+def positive_integer(value):
+    if value <= 0:
+        raise ValueError("Value is not positive")
+
+    return value
 
 
-def woww():
-    ser = serial.Serial('/pyprj/abc/COM8', write_timeout=10)
-    ser.write(b"hello")
-    msg = ser.read(5)
-    print(ser.name, ser.port, ser.baudrate, ser.bytesize, ser.is_open, msg)
-    ser.close()
+def message_validator(value):
+    if not isinstance(value, str):
+        raise ValueError("Message should be string")
+    if len(value) > 500:
+        raise ValueError("Maximum length of message is 500")
+
+    return value
+
+
+@api.resource('/ports/')
+class PortListInfo(Resource):
+
+    @jwt_required()
+    def get(self):
+        data = get_list_ports()
+        return data, 200
+
+
+@api.resource('/send-msg/')
+class SerialPortMessage(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'message',
+        type=message_validator,
+        required=True,
+        help="{error_msg}"
+    )
+
+    @jwt_required()
+    def post(self):
+        args = SerialPortMessage.parser.parse_args()
+        message1 = b"hello\nLukas\n\rHaha\nGotcha\n"
+        message = bytes(args["message"], 'ascii')
+        response_msg = write_to_serial_port(message)
+
+        return {"message": response_msg}, 200
+
+
+@api.resource('/init-port/')
+class InitSerialPort(Resource):
+    parser = reqparse.RequestParser(bundle_errors=True)
+    parser.add_argument(
+        'port',
+        type=str,
+        location='json',
+        required=True,
+        help="This field cannot be blank."
+    )
+    parser.add_argument(
+        'baudrate',
+        choices=serial.Serial.BAUDRATES,
+        type=int,
+        required=False,
+        help="Bad choice: {error_msg}." +
+        "The choices are: {}".format(serial.Serial.BAUDRATES)
+    )
+    parser.add_argument(
+        'bytesize',
+        choices=serial.Serial.BYTESIZES,
+        type=int,
+        required=False,
+        help="Bad choice: {error_msg}." +
+        "The choices are: {}".format(serial.Serial.BYTESIZES)
+    )
+    parser.add_argument(
+        'stopbits',
+        choices=serial.Serial.STOPBITS,
+        type=int,
+        required=False,
+        help="Bad choice: {error_msg}." +
+        "The choices are: {}".format(serial.Serial.STOPBITS)
+    )
+    parser.add_argument(
+        'parity',
+        choices=serial.Serial.PARITIES,
+        type=int,
+        required=False,
+        help="Bad choice: {error_msg}." +
+        "The choices are: {}".format(serial.Serial.PARITIES)
+    )
+    parser.add_argument(
+        'flow_control',
+        choices=('xonxoff', 'rtscts', 'dsrdtr'),
+        type=str,
+        required=False,
+        help="Bad choice: {error_msg}. The choices are: ('xonxoff', 'rtscts', 'dsrdtr')"
+    )
+    parser.add_argument(
+        'write_timeout',
+        type=positive_integer,
+        required=False,
+        help="Error: {error_msg}"
+    )
+    parser.add_argument(
+        'timeout',
+        type=positive_integer,
+        required=False,
+        help="Error: {error_msg}"
+    )
+
+    @jwt_required()
+    def post(self):
+        args = Collections.drop_none(InitSerialPort.parser.parse_args())
+        response_msg = init_serial_port(**args)
+        return {"message": response_msg}, 200
